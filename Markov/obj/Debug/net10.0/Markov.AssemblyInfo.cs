5fde77b516aea3a4a2db52d84b68af9cfe3ad2cb
DavidVanMeel <123325134+DavidVanMeel@users.noreply.github.com>
2026-08-28 02:36:05 +0200

    Generated files from first run

--- Markov/obj/Debug/net10.0/Markov.AssemblyInfo.cs
+++ Markov/obj/Debug/net10.0/Markov.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Markov")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+797455528e06f052e0f371720b5a4c24685d8d7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Markov")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Markov")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

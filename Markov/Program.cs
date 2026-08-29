0cc47979565de517804136126bef983ee3aeb722
DavidVanMeel <123325134+DavidVanMeel@users.noreply.github.com>
2026-08-29 22:48:16 +0200

    Markov chain v1

--- Markov/Program.cs
+++ Markov/Program.cs
@@ -2,9 +2,46 @@
 {
     internal class Program
     {
+        private const string trainingDataURL = "C:\\Users\\david\\source\\repos\\Markov\\Markov\\Training data.txt";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool exit = false;
+            string command = "";
+
+            //genereer de Markov chains o.b.v. trainingdata
+            MarkovChain wordChain = new MarkovChain(".", trainingDataURL, ".");
+            MarkovChain sentenceChain = new MarkovChain(".", trainingDataURL, "\\w+|\\W", 4);
+
+            //command line loop to allow commands
+            Console.WriteLine(">Please enter the type of output you wish to generate (\"WORDS\" or \"SENTENCES\")");
+
+            while (!exit)
+            {
+                //read and format command
+                command = Console.ReadLine();
+                if (command is null)
+                {
+                    command = "";
+                }
+                command = command.ToUpper().Trim();
+
+                //respond
+                switch (command)
+                {
+                    case "WORDS":
+                        Console.WriteLine(wordChain.GenerateString(1000));
+                        break;
+                    case "SENTENCES":
+                        Console.WriteLine(sentenceChain.GenerateString(1000));
+                        break;
+                    case "EXIT":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine(">");
+                        break;
+                }
+            }
         }
     }
 }
